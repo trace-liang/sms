2f581fb3c486f165bbd653fd8f2bfd7949db825b
kyle <javatrace@foxmail.com>
2018-04-18 10:40:41 +0800

    bug fix

--- src/main/java/com/sui/sms/SmsMain.java
+++ src/main/java/com/sui/sms/SmsMain.java
@@ -4,5 +4,7 @@ public class SmsMain {
 	public static void main(String[] args) {
 		System.out.println("init");
 		System.out.println("sms_v1.0.2_dev_20180420 develop new funtion");
+		System.out.println("sms_v1.0.2_test_20180420 bugfix");
+		
 	}
 }
