0221e1896d62ec48c0ccd58f4b91b024700f3c84
kyle <javatrace@foxmail.com>
2018-04-18 13:35:31 +0800

    合并1.0.3

--- src/main/java/com/sui/sms/SmsMain.java
+++ src/main/java/com/sui/sms/SmsMain.java
@@ -4,5 +4,9 @@ public class SmsMain {
 	public static void main(String[] args) {
 		System.out.println("init");
 		System.err.println("sms_v1.0.1_test_20180420 fix bugs");
+		System.out.println("sms_v1.0.2_dev_20180420 develop new funtion");
+		System.out.println("sms_v1.0.2_test_20180420 bugfix");
+
+		
 	}
 }
