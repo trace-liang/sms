9f99e00fa1cc9c3087f879bc62b23b287ae9daae
kyle <javatrace@foxmail.com>
2018-04-18 10:33:40 +0800

    new function

--- src/main/java/com/sui/sms/SmsMain.java
+++ src/main/java/com/sui/sms/SmsMain.java
@@ -3,5 +3,6 @@ package com.sui.sms;
 public class SmsMain {
 	public static void main(String[] args) {
 		System.out.println("init");
+		System.out.println("sms_v1.0.2_dev_20180420 develop new funtion");
 	}
 }
