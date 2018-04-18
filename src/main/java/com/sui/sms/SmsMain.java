98341d77459c523a973872d377e7f3de5766eeac
kyle <javatrace@foxmail.com>
2018-04-18 09:53:11 +0800

    bug fix

--- src/main/java/com/sui/sms/SmsMain.java
+++ src/main/java/com/sui/sms/SmsMain.java
@@ -3,5 +3,6 @@ package com.sui.sms;
 public class SmsMain {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		System.err.println("sms_v1.0.0_test_20180413 bug fix");
 	}
 }
